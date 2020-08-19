0ac55b876cd83a51db9ccfdbc3cb247f19aae08e
Gita Linkeviciute <gita.link@gmail.com>
2020-08-19 17:23:31 -0400

    Add sorting

--- mergeSort.js
+++ mergeSort.js
@@ -1,25 +1,51 @@
-function mergeArrays(arr1, arr2) {
-	const sortedArr = []
-	let pointer1 = 0
-	let pointer2 = 0
 
-	while (pointer1 < arr1.length && pointer2 < arr2.length) {
-		if (arr1[pointer1] <= arr2[pointer2]) {
-			sortedArr.push(arr1[pointer1])
-			pointer1 += 1
+// sort array using merge sort algorithm
+// merging of arrays takes O(nlogn) time,  O(n) space
+
+function mergeSort(arr) {
+	if (arr.length === 1 || arr.length === 0) {
+		return arr
+	}
+	mergeSortHelper(arr, 0, arr.length-1)
+	return arr
+}
+
+function mergeSortHelper(arr, start, end) {
+	if (start >= end) return
+
+	let middle = Math.floor((start + (end-start)/2))
+	mergeSortHelper(arr, start, middle)
+	mergeSortHelper(arr, middle+1, end)
+	merge(arr, start, middle, end)
+}
+
+
+function merge(arr, start, middle, end) {
+	const result = new Array(end-start+1)
+	let pointer1 = start
+	let pointer2 = middle+1
+	let sortedIndex = 0
+
+	while (pointer1 <= middle && pointer2 <= end) {
+		if (arr[pointer1] <= arr[pointer2]) {
+			result[sortedIndex++] = arr[pointer1++]
 		}
 		else {
-			sortedArr.push(arr2[pointer1])
-			pointer2 +=1
+			result[sortedIndex++] = arr[pointer2++]
 		}
 	}
-		while (pointer1 < arr1.length) {
-		sortedArr.push(arr1[pointer1])
-		pointer1 +=1
-	}
+	while (pointer1 <= middle){
+		result[sortedIndex++] = arr[pointer1++]
 
-	while (pointer2 < arr2.length) {
-		sortedArr.push(arr2[pointer2])
-		pointer2 +=1
 	}
-	return sortedArr
+	while (pointer2 <= end) {
+		result[sortedIndex++] = arr[pointer2++]
+	}
+	// flush sorted elements back into the original array
+	for (let i = 0; i < result.length; i++) {
+		arr[start+i] = result[i]
+	}
+}
+
+console.log(mergeSort([10, 9, 7, 3, 4, 9, 12, 12, 1, 0, -1]))
+
